c6cdcf34aa3b8615ba87426f22d0b48afa1bf6ec
Matt Wettlaufer <therivenman@users.noreply.github.com>
2021-08-15 14:33:21 -0700

    Update sync-photos.sh
    Handle sync failures with modified log message

--- sync-photos.sh
+++ sync-photos.sh
@@ -4,6 +4,8 @@ FROM_DIR=/nas-photos/frame/
 TO_DIR=/home/pi/Pictures/
 RSYNC_OPTIONS="-av --del"
 
-rsync ${RSYNC_OPTIONS} ${FROM_DIR} ${TO_DIR}
-
-echo "Last Sync: $(date)" > /home/pi/bin/sync-photos.txt
+if rsync ${RSYNC_OPTIONS} ${FROM_DIR} ${TO_DIR}; then
+	echo "Last Sync: $(date)" > /home/pi/bin/sync-photos.txt
+else
+	echo "Failed Sync: $(date)" > /home/pi/bin/sync-photos.txt
+fi
